69be7646ca476d0f1b3416ed6cb14be526b9356b
김유민 <88136758+kym2207@users.noreply.github.com>
2023-04-04 13:14:54 +0900

    Add files via upload

---
+++ 0328/0328ex1.py
@@ -0,0 +1,17 @@
+'''
+2023.03.28
+김유민
+연산자연습
+'''
+num1=10
+num2=5
+
+res1=num1*num2
+res2=num1/num2
+res3=num1//num2
+res4=num1%num2
+
+print("num1*num2=",res1)
+print("num1/num2=",res2)
+print("num1//num2=",res3)
+print("num1%num2=",res4)
---
+++ 0328/p.115_6.py
@@ -0,0 +1,18 @@
+'''
+2023.03.28
+김유민
+47과 18의 가감승제 구하는 프로그램
+'''
+
+num1=47
+num2=18
+
+res1=num1+num2
+res2=num1-num2
+res3=num1*num2
+res4=num1/num2
+
+print(num1,"과 ",num2,"의 합은 :",res1)
+print(num1,"과 ",num2,"의 차는 :",res2)
+print(num1,"과 ",num2,"의 곱은 :",res3)
+print(num1,"과 ",num2,"의 나눗셈은 :",res4)
