ec97ce8eb0d9de452eb05a3803f1fdae6483c793
김유민 <88136758+kym2207@users.noreply.github.com>
2023-05-02 13:23:20 +0900

    Add files via upload

---
+++ 0502/0502ex2.py
@@ -0,0 +1,18 @@
+'''
+2023-05-02
+김유민
+range( )함수-순차적인 범위의 숫자 생성하는 
+'''
+for i in range(10) : #반복 조건
+        print(i,end=' ') #반복 조건 참인 동안 반복
+print()
+for i in range(1,10) : #반복 조건
+        print(i,end=' ') #반복 조건 참인 동안 반복
+print()
+for i in range(1,11,2):
+        print(i,end=' ')#홀수 출력
+print()
+for num in range(10,0,-1):
+        print(num,end=' ')#역순출력
+
+    
