2d46470246c435dca60e1244e510099ca3864413
김유민 <88136758+kym2207@users.noreply.github.com>
2023-05-31 15:20:34 +0900

    Add files via upload

---
+++ 0531/0531ex3.py
@@ -0,0 +1,26 @@
+'''
+2023-05-31
+김유민
+#문제정의
+    두 개의 튜플을 하나의 리스트로 합치기
+#문제분석
+    변수-빈리스트(fp_list)
+#알고리즘
+    1.튜플정의
+    2.빈 리스트 생성
+    3.반복(두 개의 튜플을 하나의 리스트로 합치기)
+        for i in range(len(fruit))
+            fruit[i]->fp_list추가
+            prince[i]->fp_list추가
+'''
+fruit=('사과','배','파인애플','포도')
+prince=(5000,7000,4500,6000)
+
+fp_list=[]
+
+for i in range(len(fruit)):
+    fp_list.append(fruit[i])
+    fp_list.append(prince[i])
+print("과일:",fruit)
+print("가격:",prince)
+print("과일+가격 리스트:",fp_list)
